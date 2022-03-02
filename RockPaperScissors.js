ff20090e2b744844c55780fb841e077c82f3f69f
BC <admin@superzen.org>
2022-03-02 09:25:52 -0700

    Update RockPaperScissors.js

--- RockPaperScissors.js
+++ RockPaperScissors.js
@@ -1,1 +1,53 @@
-//todo
+const computerChoiceDisplay = document.getElementById('computer-choice')
+const userChoiceDisplay = document.getElementById('user-choice')
+const resultDisplay = document.getElementById('result')
+const possibleChoices = document.querySelectorAll('button')
+let userChoice
+let computerChoice
+
+possibleChoices.forEach(possibleChoice => possibleChoice.addEventListener('click', (e) => {
+    userChoice =e.target.id
+    userChoiceDisplay.innerHTML = userChoice
+    generateComputerChoice()
+    getResult()
+}))
+
+function generateComputerChoice() {
+    const randomNumber = Math.floor(Math.random() * 3) + 1 //alt possibleChoices.lenth?
+    
+    if (randomNumber === 1){
+        computerChoice = 'rock'
+    }
+    if (randomNumber === 2) {
+        computerChoice = 'scissors'
+    }
+    if (randomNumber === 3) {
+        computerChoice = 'paper'
+    }
+    computerChoiceDisplay.innerHTML = computerChoice
+}
+
+function getResult() {
+    if (computerChoice === userChoice) {
+        result = 'its a tie!'
+    }
+    if (computerChoice === 'rock' && userChoice === "paper") {
+        result = 'you won!'
+    }
+    if (computerChoice === 'rock' && userChoice === "scissors") {
+        result = 'you lost!'
+    }
+    if (computerChoice === 'paper' && userChoice === "scissors") {
+        result = 'you won!'
+    }
+    if (computerChoice === 'paper' && userChoice === "rock") {
+        result = 'you lost!'
+    }
+    if (computerChoice === 'scissors' && userChoice === "rock") {
+        result = 'you won!'
+    }
+    if (computerChoice === 'scissors' && userChoice === "paper") {
+        result = 'you lost!'
+    }
+    resultDisplay.innerHTML = result
+} 
